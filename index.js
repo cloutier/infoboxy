89b80b075b82547e50f2f8b2d459f28877d8fece
Vincent Cloutier <vincent@cloutier.co>
2018-08-26 16:37:15 -0400

    started functions to get data from infoboxes

--- index.js
+++ index.js
@@ -3,11 +3,17 @@ var MediaWiki = require("mediawiki");
 var bot = new MediaWiki.Bot();
 
 bot.settings.endpoint = "https://fr.wikipedia.org/w/api.php";
+bot.settings.userAgent = "Infoboxy <https://fr.wikipedia.org/wiki/Utilisateur:Vincent_cloutier>";
 
+//getInfoboxCode( "Modèle:Infobox_Préfecture_du_Japon")
+getWikicode( "Préfecture_de_Chiba", "24270")
+getInfoboxCode( "batman")
+getInfoboxCode( "montreal")
 getAllPagesWithInfobox( "Modèle:Infobox_Préfecture_du_Japon", function (res) {
     console.log(res);
     console.log(res.length);
 })
+/*
 getAllPagesWithInfobox( "Modèle:Infobox_Municipalité_du_Canada", function (res) {
     console.log(res);
     console.log(res.length);
@@ -18,6 +24,25 @@ wiki().page('Batman')
     .then(console.log); // Bruce Wayne
     */
 
+function getPageInfobox(name) {
+    wiki({ apiUrl: 'https://fr.wikipedia.org/w/api.php' }).page(name)
+	.then(page => page.fullInfo())
+	.then(console.log); // Bruce Wayne
+
+}
+function getWikicode(name, pageid) {
+    bot.get({ action: "query", titles: name, "prop": "revisions", rvprop: "content", "rvslots": "main" }).complete(function (response) {
+	console.log(response)
+	console.log(response.query.pages[pageid].revisions[0].slots.main["*"])
+    });
+
+}
+function getInfoboxCode(name) {
+    wiki({ apiUrl: 'https://fr.wikipedia.org/w/api.php' }).page(name)
+	.then(page => page.fullInfo())
+	.then(console.log); // Bruce Wayne
+
+}
 function getAllPagesWithInfobox(name, sucess) {
     bot.get({ action: "query", list: "embeddedin", eititle: name, eilimit: 500 }).complete(function (response) {
 	results = [];

5acc5dea5486a7d6f619a22c125459f2c439866b
Vincent Cloutier <vincent@cloutier.co>
2018-08-28 15:24:35 -0400

    can now get unparsed code for an infobox

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
 const wiki = require('wikijs').default;
 var MediaWiki = require("mediawiki");
 var bot = new MediaWiki.Bot();
+var wtf = require('wtf_wikipedia');
+const request = require('request');
 
 bot.settings.endpoint = "https://fr.wikipedia.org/w/api.php";
 bot.settings.userAgent = "Infoboxy <https://fr.wikipedia.org/wiki/Utilisateur:Vincent_cloutier>";
 
-//getInfoboxCode( "Modèle:Infobox_Préfecture_du_Japon")
-getWikicode( "Préfecture_de_Chiba", "24270")
-getInfoboxCode( "batman")
-getInfoboxCode( "montreal")
+getInfoboxCode( "Modèle:Infobox_Préfecture_du_Japon")
+//getWikicode( "'Modèle:Infobox_Municipalité_du_Canada")
+//getWikicode( "Préfecture_de_Chiba", "24270")
+//getInfoboxCode( "batman")
+//getInfoboxCode( "montreal")
 getAllPagesWithInfobox( "Modèle:Infobox_Préfecture_du_Japon", function (res) {
-    console.log(res);
+    //console.log(res);
     console.log(res.length);
 })
-/*
-getAllPagesWithInfobox( "Modèle:Infobox_Municipalité_du_Canada", function (res) {
-    console.log(res);
-    console.log(res.length);
-});
-/*
-wiki().page('Batman')
-    .then(page => page.info('alterEgo'))
-    .then(console.log); // Bruce Wayne
-    */
 
+wtf.fetch('Montreal', 'fr', function(err, doc) {
+    var data = doc.infobox(0).data
+    console.log(data);
+});
 function getPageInfobox(name) {
     wiki({ apiUrl: 'https://fr.wikipedia.org/w/api.php' }).page(name)
 	.then(page => page.fullInfo())
@@ -33,14 +30,18 @@ function getPageInfobox(name) {
 function getWikicode(name, pageid) {
     bot.get({ action: "query", titles: name, "prop": "revisions", rvprop: "content", "rvslots": "main" }).complete(function (response) {
 	console.log(response)
-	console.log(response.query.pages[pageid].revisions[0].slots.main["*"])
+	console.log(response.query.pages)
+	console.log(response.query.pages["-1"].revisions[0].slots.main["*"])
     });
 
 }
 function getInfoboxCode(name) {
-    wiki({ apiUrl: 'https://fr.wikipedia.org/w/api.php' }).page(name)
-	.then(page => page.fullInfo())
-	.then(console.log); // Bruce Wayne
+    request('https://fr.wikipedia.org/w/index.php?action=raw&title=' + name, (err, res, body) => {
+	if (err) { console.log(err); }
+	console.log(res.body);
+	console.log(body.url);
+	console.log(body.explanation);
+    });
 
 }
 function getAllPagesWithInfobox(name, sucess) {
